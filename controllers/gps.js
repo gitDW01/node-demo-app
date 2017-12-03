84c2c912acfc155e89f1dc8c677741940d65e8c9
gitDW01 <maildw01@gmail.com>
2017-12-03 19:18:09 +0200

    Update gps.js

--- controllers/gps.js
+++ controllers/gps.js
@@ -4,7 +4,7 @@ var processing = function(req, res){
   if (!req.body) return res.sendStatus(400)
   var str = req.body
 
-  global.outlol = str.arduino
+  //global.outlol = str.arduino
   
   res.send('welcome' + ' ' + str.arduino)
 }

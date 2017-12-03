e191d48f6c3d43a9a2a1ba5f84b4ffd56fff1925
gitDW01 <maildw01@gmail.com>
2017-12-03 18:27:23 +0200

    Update gps.js

--- controllers/gps.js
+++ controllers/gps.js
@@ -1,21 +1,11 @@
 "use strict"
 
-var mysql = require('mysql')
-  , connection = mysql.createConnection({ host : 'localhost', user : 'navigator', password : 'Q16061995', database : 'gps' });
-
-
-
 var processing = function(req, res){
   if (!req.body) return res.sendStatus(400)
   var str = req.body
-  var query = connection.query('INSERT INTO telemetry SET ?', str, function(err, result) {
-    if (err) throw err
-  console.log(err)
- console.log(result)
-})
 
   console.log(query.sql)
-  res.send('welcome' + str.name)
+  res.send('welcome' + str)
 }
 
 

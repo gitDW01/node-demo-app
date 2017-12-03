8a22678bcb2ae9fb8205d4850cac282b9b77ceec
gitDW01 <maildw01@gmail.com>
2017-12-03 20:33:15 +0200

    Update pages.js

--- controllers/pages.js
+++ controllers/pages.js
@@ -1,14 +1,14 @@
 var home = function (req, res) {
   res.render('pages/home', {
     title: 'Home page'
-  , message: 'Number of cycles' global.outlol
+  , message: "Number of cycles + global.outlol"
 })
 }
 
 var about = function (req, res) {
   res.render('pages/about', {
     title: 'About'
-  , message: 'This is the "about" action of "pages" controller'
+  , message: 'This is the demo project of Yaraslau Klimkou and Nickolay Rudenko'
 })
 }
 
